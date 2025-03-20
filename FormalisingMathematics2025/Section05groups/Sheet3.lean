cab6328e44fefdf2cdf0826c4d7296f36806233b
Dan Abramov <dan.abramov@gmail.com>
2025-03-21 03:04:47 +0900

    Solutions for Section05/Sheet3

--- FormalisingMathematics2025/Section05groups/Sheet3.lean
+++ FormalisingMathematics2025/Section05groups/Sheet3.lean
@@ -37,7 +37,11 @@ example (a b : G) (ha : a ∈ H) (hb : b ∈ H) : a * b ∈ H := by
 
 example (a b c : G) (ha : a ∈ H) (hb : b ∈ H) (hc : c ∈ H) :
     a * b⁻¹ * 1 * (a * c) ∈ H := by
-  sorry
+  apply H.mul_mem
+  · apply H.mul_mem
+    · exact H.mul_mem ha (H.inv_mem hb)
+    · exact H.one_mem
+  · apply H.mul_mem ha hc
 
 /-
 
@@ -68,7 +72,9 @@ example (H K : Subgroup G) (a : G) : a ∈ H ⊓ K ↔ a ∈ H ∧ a ∈ K := by
 -- Note that `a ∈ H ⊔ K ↔ a ∈ H ∨ a ∈ K` is not true; only `←` is true.
 -- Take apart the `Or` and use `exact?` to find the relevant lemmas.
 example (H K : Subgroup G) (a : G) : a ∈ H ∨ a ∈ K → a ∈ H ⊔ K := by
-  sorry
+  rintro (ah | ak)
+  · exact Subgroup.mem_sup_left ah
+  · exact Subgroup.mem_sup_right ak
 
 end Subgroups
 
@@ -97,13 +103,14 @@ example (a b : G) : φ (a * b⁻¹ * 1) = φ a * (φ b)⁻¹ * 1 := by
   -- if `φ.map_mul` means that `φ` preserves multiplication
   -- (and you can rewrite with this) then what do you think
   -- the lemmas that `φ` preserves inverse and one are called?
-  sorry
+  rw [φ.map_mul, φ.map_mul, φ.map_inv, φ.map_one]
 
 -- Group homomorphisms are extensional: if two group homomorphisms
 -- are equal on all inputs then they're the same.
 
 example (φ ψ : G →* H) (h : ∀ g : G, φ g = ψ g) : φ = ψ := by
   -- Use the `ext` tactic.
-  sorry
+  ext g
+  exact h g
 
 end Homomorphisms
